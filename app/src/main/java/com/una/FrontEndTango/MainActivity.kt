172ddeda241ee10397160ab71ff7d87325bc1a74
aragonj1112 <126797104+aragonj1112@users.noreply.github.com>
2023-04-03 13:07:35 -0600

    Cambio de nombre de Activities
    - El Activity de inicio es ahora LoginActivity
    - El Activity donde se hace el display de fragments es ahora el MainActivity

--- app/src/main/java/com/una/FrontEndTango/MainActivity.kt
+++ app/src/main/java/com/una/FrontEndTango/MainActivity.kt
@@ -1,52 +1,27 @@
 package com.una.FrontEndTango
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.una.FrontEndTango.databinding.ActivityMainBinding
-
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding // creo mi variable que utiliza el metodo ActivityMainBinding que me remite a al layout activity_main
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // Seleccionar el fragment de navegacion inicial para utilizar navController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.app_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
-        //setContentView(R.layout.activity_main), en lugar de usar este se usa lo de arriba con el binding
-
-
-        binding.btLogin.setOnClickListener{   //Click boton login
-
-            //traemos valores de imput
-            val email = binding.etEmail.text.toString().trim() //trim() es para eliminar espacios extra si los hubiera
-            val password = binding.etPassword.text.toString().trim()
-
-            //validacion
-
-            if(email.isEmpty()){ //si esta vacio devuelve true
-
-                binding.etEmail.error = "Enter Email" //msj de error
-                binding.etEmail.requestFocus() // para seleccionar el campo
-
-            }else if( password.isEmpty()) {
-
-                binding.etPassword.error = "Enter Password"
-                binding.etPassword.requestFocus()
-
-            }else{ //los dos campos estan llenos
-
-                //abre el vista de dashboard
-                val intent = Intent(this,DashboardActivity::class.java)
-                startActivity(intent)
-
-                finish()
-            }
-        }
-
+        // Navegacion de la barra de abajo
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 }
