5af7d32c898f9580139feab23df275f235a39f49
oKamix <126527588+NachoGreen@users.noreply.github.com>
2023-04-09 16:05:10 -0600

    Funcionalidad Botones Menu Estudiante

--- app/src/main/java/com/una/FrontEndTango/Fragments/MenuEstudiante.kt
+++ app/src/main/java/com/una/FrontEndTango/Fragments/MenuEstudiante.kt
@@ -5,56 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.navigation.fragment.findNavController
 import com.una.FrontEndTango.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MenuEstudiante.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MenuEstudiante : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_estudiante, container, false)
+        val view = inflater.inflate(R.layout.fragment_menu_estudiante, container, false)
+
+        //Boton My Units
+        val botonMyUnits:ImageButton=view.findViewById(R.id.btMyUnits)
+
+        //Funcionalidad Boton My Units
+        botonMyUnits.setOnClickListener{
+            // Hacer la navegacion de un fragment a otro, segun ruta encontrada en grafico de navegacion
+            findNavController().navigate(R.id.action_menuEstudiante_to_activeUnits)
+        }
+
+        //Boton Create Request
+        val botonCreateRequest:ImageButton=view.findViewById(R.id.btCreateRequest)
+
+        //Funcionalidad Boton Create Request
+        botonCreateRequest.setOnClickListener{
+            // Hacer la navegacion de un fragment a otro, segun ruta encontrada en grafico de navegacion
+            findNavController().navigate(R.id.action_menuEstudiante_to_unitRequest)
+        }
+
+        //Boton Ticket User
+        val botonTicketUser:ImageButton=view.findViewById(R.id.btTicketUser)
+
+        //Funcionalidad Boton Ticket User
+        botonTicketUser.setOnClickListener{
+            // Hacer la navegacion de un fragment a otro, segun ruta encontrada en grafico de navegacion
+            findNavController().navigate(R.id.action_menuEstudiante_to_createTicket)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuEstudiante.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuEstudiante().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
