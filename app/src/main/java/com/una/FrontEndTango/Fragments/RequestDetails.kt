ea469f5bc3d72814adcb2e96fa8b4f100662a370
oKamix <126527588+NachoGreen@users.noreply.github.com>
2023-04-09 12:57:39 -0600

    Request Details agregado a nav_graph.xml Funcionalidad temporal a boton deny

--- app/src/main/java/com/una/FrontEndTango/Fragments/RequestDetails.kt
+++ app/src/main/java/com/una/FrontEndTango/Fragments/RequestDetails.kt
@@ -5,56 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.una.FrontEndTango.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RequestDetails.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RequestDetails : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_request_details, container, false)
+        val view = inflater.inflate(R.layout.fragment_request_details, container, false)
+
+        //Boton Deny
+        val botonDeny:Button = view.findViewById(R.id.btnDeny)
+
+        //Funcionalidad temporal Boton Deny
+        botonDeny.setOnClickListener{
+            // Hacer la navegacion de un fragment a otro, segun ruta encontrada en grafico de navegacion
+            findNavController().navigate(R.id.action_requestDetails_to_requestsGuarda)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RequestDetails.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RequestDetails().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
