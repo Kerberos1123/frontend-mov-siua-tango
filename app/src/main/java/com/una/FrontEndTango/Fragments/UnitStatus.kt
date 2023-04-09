7066cc57897174a0f8e91c6819e54ba0a9e9708d
aragonj1112 <126797104+aragonj1112@users.noreply.github.com>
2023-04-08 19:13:28 -0600

    Fragment Unit Status
    - Se hizo el diseño del fragment de unit status/progress

--- app/src/main/java/com/una/FrontEndTango/Fragments/UnitStatus.kt
+++ app/src/main/java/com/una/FrontEndTango/Fragments/UnitStatus.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.una.FrontEndTango.R
 
 
@@ -16,6 +18,17 @@ class UnitStatus : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_unit_status, container, false)
+
+        // --- Boton Back ---
+        // Variable donde tenemos el boton
+        val botonBack: Button = view.findViewById(R.id.back_button)
+
+        // Hacer funcion del boton tras hacerle click
+        botonBack.setOnClickListener{
+            // Hacer la navegacion de un fragment a otro, segun ruta encontrada en grafico de navegacion
+            findNavController().navigate(R.id.action_unitStatus_to_clasesProfe)
+        }
+        // --- -------------- ---
 
         return view
     }
