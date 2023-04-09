45c1269c060dc01a4ccb29a26e2a1a8822561c2b
oKamix <126527588+NachoGreen@users.noreply.github.com>
2023-04-09 12:37:20 -0600

    Funcionalidad Botones: *changePassword de ProfileMeu *cancel de ChangePassword

--- app/src/main/java/com/una/FrontEndTango/Fragments/ProfileMenu.kt
+++ app/src/main/java/com/una/FrontEndTango/Fragments/ProfileMenu.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.una.FrontEndTango.R
 
 class ProfileMenu : Fragment() {
@@ -14,7 +16,19 @@ class ProfileMenu : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_menu, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile_menu, container, false)
+
+        //Boton Change Password
+        val botonChangePassword:Button=view.findViewById(R.id.btnChangePassword)
+
+        //Funcionalidad Boton Change Password
+        botonChangePassword.setOnClickListener{
+            // Hacer la navegacion de un fragment a otro, segun ruta encontrada en grafico de navegacion
+            findNavController().navigate(R.id.action_profileMenu_to_changePassword)
+        }
+
+
+        return view
     }
 
 }
