476f1e8d39e74e9627ec78715e3625e3addae997
oKamix <126527588+NachoGreen@users.noreply.github.com>
2023-04-09 14:40:39 -0600

    Fragment PopUp Request

---
+++ app/src/main/java/com/una/FrontEndTango/Fragments/RequestPopUp.kt
@@ -0,0 +1,20 @@
+package com.una.FrontEndTango.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.una.FrontEndTango.R
+
+class RequestPopUp : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_request_pop_up, container, false)
+    }
+
+}
