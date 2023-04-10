5603d8b5239d5000d7af162913a28c897aa4d25f
aragonj1112 <126797104+aragonj1112@users.noreply.github.com>
2023-04-10 02:01:50 -0600

    Seleccion de fragment inicial
    -Se agrego un selector del fragment inicial que variara segun el tipo de usuario que ingrese

--- app/src/main/java/com/una/FrontEndTango/LoginActivity.kt
+++ app/src/main/java/com/una/FrontEndTango/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.una.FrontEndTango
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.una.FrontEndTango.databinding.ActivityLoginBinding
 
 
