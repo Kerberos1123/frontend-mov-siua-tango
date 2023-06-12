9f8791e94dcad081be8a8da168e7b46a23c2c1f4
aragonj1112 <126797104+aragonj1112@users.noreply.github.com>
2023-06-12 10:00:10 -0600

    Clases Admin
    Funcionamiento del recycler de clases del menu de administrador

--- app/src/main/java/com/una/FrontEndTango/View/Admin/ClasesAdmin.kt
+++ app/src/main/java/com/una/FrontEndTango/View/Admin/ClasesAdmin.kt
@@ -1,98 +1,77 @@
 package com.una.FrontEndTango.View.Admin
 
+//IMPORTS VIEJOS
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.una.FrontEndTango.Adapter.ClassAdapter
 import com.una.FrontEndTango.R
-import com.una.FrontEndTango.Adapter.RecyclerAdapterClass
-import com.una.FrontEndTango.Model.Class
+import com.una.FrontEndTango.ViewModel.ClassViewModelFactory
+
+//IMPORTS NUEVOS
+import com.una.FrontEndTango.databinding.FragmentClasesAdminBinding
+import com.una.FrontEndTango.ViewModel.ClassViewModel
+import com.una.FrontEndTango.ViewModel.StateClass
 
 class ClasesAdmin : Fragment() {
 
-    //variables de referencia
-    private lateinit var adapter : RecyclerAdapterClass
-    private lateinit var recyclerView: RecyclerView
+    private var _binding: FragmentClasesAdminBinding? = null
+    private val binding get() = _binding!!
 
-    private lateinit var classArrayList: ArrayList<Class>
-
-    //Arrays para titulos y descripciones
-    lateinit var titulo : Array<String>
-    lateinit var descripcion :Array<String>
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clases_admin, container, false)
-
+    private val classViewModel: ClassViewModel by activityViewModels{
+        ClassViewModelFactory()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val adapter = ClassAdapter()
 
-        //se inicializa data
-        dataInitialize()
+        _binding = FragmentClasesAdminBinding.inflate(inflater,container,false)
 
-        //seteamos parametros del recycle view y su layout manager
+        binding.recyclerClasesAdmin.adapter = adapter
+
         val layoutManager = LinearLayoutManager(context)
+        binding.recyclerClasesAdmin.layoutManager = layoutManager
 
-        recyclerView = view.findViewById(R.id.recycler_clases_admin)
+        //traemos todas las requests del webservice
+        classViewModel.findAllClass()
+        classViewModel.state.observe(viewLifecycleOwner) { state ->
 
-        recyclerView.layoutManager = layoutManager
-        recyclerView.setHasFixedSize(true)
-        adapter = RecyclerAdapterClass(classArrayList)
-        recyclerView.adapter = adapter
-
-        // Agregar el click listener
-        adapter.setOnItemClickListener(object: RecyclerAdapterClass.onItemClickListener{
-            override fun onItemClick(position: Int) { // Implementar logica al hacer click en seccion de recycler
-                findNavController().navigate(R.id.action_clasesAdmin_to_createClass1)
+            when (state) {
+                StateClass.Loading -> {
+                    // TODO: If you need do something in loading
+                }
+                is StateClass.Error -> {
+                    // TODO: If you need do something in error
+                }
+                is StateClass.SuccessList -> {
+                    state.classList?.let { adapter.setClassList(it) }
+                }
+                else -> {
+                    // TODO: Not state loaded
+                }
             }
-        })
-        // --- ---
+
+        }
 
         // --- Boton CreateClass
-        val botonCreateClass: Button = view.findViewById(R.id.btCreateClass)
+        val botonCreateClass: Button = binding.btCreateClass
         botonCreateClass.setOnClickListener {
             findNavController().navigate(R.id.action_clasesAdmin_to_createClass1)
         }
 
+        return binding.root
+
     }
-
-    private fun dataInitialize(){ //inicializacion de datos
-
-
-        classArrayList = arrayListOf<Class>()    //se crea arraylist que almacena el contenido de las clases
-
-        titulo = arrayOf(
-            //se inicializan datos de los titulos
-
-            getString(R.string.T2), getString(R.string.T3)
-        )
-        descripcion = arrayOf( //se inicializan datos de los descripcion
-
-            getString(R.string.D2), getString(R.string.D3)
-        )
-
-        for ( i in titulo.indices){
-
-
-            //creamos objeto clase
-
-            val `class` = Class(titulo[i],descripcion[i])
-
-            //se añade a la lista
-            classArrayList.add(`class`)
-        }
-    }
-
-
 }
